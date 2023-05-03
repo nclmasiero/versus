12c4eb99832c4b47f1fae04b47a834db2426bc5f
nclmasiero <nclmasiero@gmail.com>
2023-05-03 18:57:54 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(51);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
